c979867cf48c68c2e2b8933bbfa2cdacfb3d630b
Karim Ali Saber <KSaber@octiga.com>
2024-01-28 04:44:47 +0200

    set router link options

--- src/app/modules/stations/stations.module.ts
+++ src/app/modules/stations/stations.module.ts
@@ -1,27 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StationsRoutingModule } from './stations-routing.module';
-import { ChartComponent } from './components/chart/chart.component';
-import { MapComponent } from './components/map/map.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { DetailsComponent } from './components/details/details.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { HomeComponent } from './components/home/home.component';
-import { SharedModule } from '../shared/shared.module';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { NgChartsModule } from 'ng2-charts';
-import { SeverityBarComponent } from './components/severity-bar/severity-bar.component';
-import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NzRadioModule } from 'ng-zorro-antd/radio';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { DetailedChartComponent } from './components/detailed-chart/detailed-chart.component';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NgChartsModule } from 'ng2-charts';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../shared/shared.module';
+import { ChartComponent } from './components/chart/chart.component';
+import { DetailedChartComponent } from './components/detailed-chart/detailed-chart.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { SeverityBarComponent } from './components/severity-bar/severity-bar.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { StationsRoutingModule } from './stations-routing.module';
 
 @NgModule({
   declarations: [
