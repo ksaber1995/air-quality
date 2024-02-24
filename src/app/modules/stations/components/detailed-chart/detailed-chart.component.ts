99fc5743f0eb1342bb80d251e6e93083fda6296b
Ksaber Saber <ksaber@octiga.com>
2024-02-24 16:50:36 +0200

    populate all data

--- src/app/modules/stations/components/detailed-chart/detailed-chart.component.ts
+++ src/app/modules/stations/components/detailed-chart/detailed-chart.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ChartData, ChartDataset, ChartOptions } from 'chart.js';
+import { BehaviorSubject, combineLatest, filter, map, shareReplay, switchMap } from 'rxjs';
+import { DetailedStation, Reading, Station } from '../../../../models/Station';
+import { BreakPoint } from '../../../../models/breakPoint';
+import { HistoryInterval, OverviewType, SwaggerService } from '../../../../services/swagger.service';
+import { OmmanDate, formatDateYYMMDD, formatTime, getDayName } from '../../../../unitlize/custom-date';
 import { Levels } from '../../../shared/model/severity';
 import { getRandomNumber } from '../summary/model';
-import { DetailedStation, Station } from '../../../../models/Station';
-import { HistoryInterval, OverviewType, SwaggerService } from '../../../../services/swagger.service';
-import { combineLatest, map, shareReplay, interval, BehaviorSubject, switchMap } from 'rxjs';
-import { OmmanDate, formatDateYYMMDD, formatTime, getDayName } from '../../../../unitlize/custom-date';
-import { BreakPoint } from '../../../../models/breakPoint';
 
 
 
@@ -28,13 +28,13 @@ export class DetailedChartComponent {
 
 
   activeInterval$ = new BehaviorSubject<HistoryInterval>('day')
+  type$ = new BehaviorSubject<string>('aqi')
 
   interval: HistoryInterval = 'day';
 
-  type;
+  filter_type;
   date: Date;
   getRandomNumber = getRandomNumber
-  data = Levels;
   downloadType = 'pdf'
 
   breakPoints: BreakPoint[] = [];
@@ -74,15 +74,7 @@ export class DetailedChartComponent {
   public lineChartLabels: string[];
   public doughnutChartLabels: string[] = Levels.map(res => res.name);
 
-  public doughnutChartData: ChartData<'doughnut'> = {
-    labels: this.doughnutChartLabels,
-    datasets: [{
-
-      // label: 'AQI',
-      data: Levels.map(res => getRandomNumber(500)),
-      backgroundColor: Levels.map(res => res.color),
-    }]
-  };
+  public doughnutChartData: ChartData<'doughnut'> ;
 
   public lineChartOptions: ChartOptions = {
     responsive: true,
@@ -121,9 +113,10 @@ export class DetailedChartComponent {
 
     },
   };
-  history: any[];
+  history: Reading[];
 
-
+  summary: { [key: string]: Reading[] } = {}
+  summaryKeys: { name: string; percentage: number; color: string; }[] = [];
 
 
   constructor(private swagger: SwaggerService) {
@@ -137,20 +130,41 @@ export class DetailedChartComponent {
   ngOnInit(): void {
     const breakPoints$ = this.swagger.getBreakPoints().pipe(map(res => res.aqi_breakpoints?.sort((a, b) => a.sequence - b.sequence))).pipe(shareReplay())
 
-    combineLatest([this.activeInterval$])
-      .pipe(switchMap(([interval]) => {
+    combineLatest([this.activeInterval$, this.type$])
+      .pipe(switchMap(([interval, type]) => {
         this.interval = interval;
+         this.filter_type = type === 'aqi' ? 'aqi' : 'variable';
 
-        return combineLatest([this.getHistory(interval, 'aqi'), breakPoints$])
+        return combineLatest([this.getHistory(interval, this.filter_type), breakPoints$])
       }))
       .subscribe(([history, breakPoints]) => {
         this.history = history;
+        console.log(history, 'koko')
         this.breakPoints = breakPoints
+        
+        this.summary = {}
+        if (this.filter_type === 'aqi') {
+
+          this.history.forEach(item => {
+            const status = item.status_en || 'na'
+
+            if (this.summary[status]) {
+              this.summary[status].push(item)
+            }
+            else {
+              this.summary[status] = [item]
+            }
+          })
+        }
+
+        console.log(this.summary, 'summary')
+
+
+        this.setDoughnutChartData()
+
 
         if (this.interval === 'day' || this.interval === 'week') {
           this.lineChartLabels = this.history.map(res => getDayName(OmmanDate(res.aggregated_at).getDay()) + formatTime(OmmanDate(res.aggregated_at)))
-
-
         }
 
         else if (this.interval === 'month') {
@@ -178,6 +192,24 @@ export class DetailedChartComponent {
 
       })
   }
+  setDoughnutChartData() {
+    const keys = Object.keys(this.summary) 
+    const backgroundColor = keys.map(key => this.history.find(res=> res.status_en === key)?.color || 'rgb(231, 231, 231)' )
+    this.summaryKeys = keys.map((key,i)=>({name: key, percentage: this.summary[key].length  / this.history.length * 100, color: backgroundColor[i] } ));
+    const data = keys.map(key=> this.summary[key].length)
+
+    this.doughnutChartData =
+    {
+      labels: keys,
+      datasets: [{
+  
+        label: 'AQI',
+        data,
+        
+        backgroundColor,
+      }]
+    };
+  }
 
   onDateChange() {
 
@@ -187,5 +219,4 @@ export class DetailedChartComponent {
     this.activeInterval$.next(interval)
 
   }
-
 }
